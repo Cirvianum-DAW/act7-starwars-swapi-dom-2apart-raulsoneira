e9149b892874824fdd0e47c67d0fdfadf637429c
Raul Soneira <raul.soneira@cirvianum.cat>
2024-04-02 17:56:08 +0200

    ex3 fet i funcional

--- src/act7.js
+++ src/act7.js
@@ -1,82 +1,75 @@
-import swapi from './swapi.js';
+import swapi from "./swapi.js";
 
 //Exemple d'inicialització de la llista de pel·lícules. Falten dades!
-async function setMovieHeading(movieId, titleSelector, infoSelector, directorSelector) {
+async function setMovieHeading(
+  movieId,
+  titleSelector,
+  infoSelector,
+  directorSelector
+) {
   // Obtenim els elements del DOM amb QuerySelector
   const title = document.querySelector(titleSelector);
   const info = document.querySelector(infoSelector);
   const director = document.querySelector(directorSelector);
 
+  if (!movieId) {
+    title.innerHTML = " ";
+    info.innerHTML = " ";
+    director.innerHTML = " ";
+    return
+  }
+
   // Obtenim la informació de la pelicula
   const movieInfo = await swapi.getMovieInfo(movieId);
   // Injectem
   title.innerHTML = movieInfo.name;
-  info.innerHTML = `Episode ${movieInfo.episodeID} - ${movieInfo.release}`
+  info.innerHTML = `Episode ${movieInfo.episodeID} - ${movieInfo.release}`;
   director.innerHTML = "Director: " + movieInfo.director;
 }
 
 async function initMovieSelect(selector) {
-  const select = document.querySelector(selector);
-
-  const defaultOption = document.createElement('option');
-  defaultOption.value = '';
-  defaultOption.textContent = 'Selecciona una pel·lícula';
-  select.appendChild(defaultOption);
-
   const movies = await swapi.listMoviesSorted();
 
+  const select = document.querySelector(selector);
+
+  const option = document.createElement("option");
+
+  option.value = "";
+  option.textContent = "Selecciona una pel·lícula";
+  select.appendChild(option);
+
   movies.forEach((movie) => {
-    const option = document.createElement('option');
-    option.value = movie.episodeID; // Considerant que l'ID de la pel·lícula és l'episodeID
+    const option = document.createElement("option");
+    option.value = _filmIdToEpisodeId(movie.episodeID);
     option.textContent = movie.name;
     select.appendChild(option);
   });
-
 }
 
+// Defineix la funció per establir els callbacks del selector de pel·lícules
 function setMovieSelectCallbacks() {
   // Obtenim el selector de pel·lícules
-  const movieSelect = document.querySelector('#select-movie');
+  const movieSelect = document.querySelector("#select-movie");
 
   // Afegim un event listener per a l'esdeveniment change
-  movieSelect.addEventListener('change', async () => {
-    // Obtenim l'episodeID de la pel·lícula seleccionada
-    const selectedEpisodeID  = movieSelect.value;
-    console.log(selectedEpisodeID);
+  movieSelect.addEventListener("change", _handleOnSelectMovieChanged);
+}
+async function _handleOnSelectMovieChanged(event) {
+  // Obtenim el valor seleccionat
+  const movieID = event.target.value;
 
-
-
-    // Obtenim la capçalera de la pel·lícula
-    const titleElement = document.querySelector('.movie__title');
-    const infoElement = document.querySelector('.movie__info');
-    const directorElement = document.querySelector('.movie__director');
-
-    // Si s'ha seleccionat una pel·lícula
-    if (selectedEpisodeID) {
-      try {
-        // Obtenim la informació de la pel·lícula seleccionada
-        const movieInfo = await swapi.getMovieInfo(selectedEpisodeID);
-        //console.log(movieInfo);
-
-        // Actualitzem la capçalera amb la informació de la pel·lícula
-        titleElement.textContent = movieInfo.name;
-        infoElement.textContent = `Episode ${movieInfo.episodeID} - ${movieInfo.release}`;
-        directorElement.textContent = "Director: " + movieInfo.director;
-      } catch (error) {
-        console.error('Error al obtenir la informació de la pel·lícula:', error);
-      }
-    } else {
-      // Si s'ha seleccionat "Selecciona una pel·lícula", buidem la capçalera
-      titleElement.textContent = '';
-      infoElement.textContent = '';
-      directorElement.textContent = '';
-    }
-  });
+    await setMovieHeading(
+      movieID,
+      ".movie__title",
+      ".movie__info",
+      ".movie__director"
+    );
 }
 
-
+// Funció per eliminar totes les fitxes de personatges
 function deleteAllCharacterTokens() {
-  
+  const characterTokens = document.querySelectorAll(".list__item.character");
+  characterTokens.forEach((token) => token.remove());
 }
 
 // EVENT HANDLERS //
@@ -87,11 +80,15 @@ async function _createCharacterTokens() {}
 
 function _addDivChild(parent, className, html) {}
 
+function _filmIdToEpisodeId(episodeID) {
+  const mapping = episodeToMovieIDs.find((item) => item.e === episodeID);
 
-
-async function _handleOnSelectMovieChanged(event) {}
-
-function _filmIdToEpisodeId(episodeID) {}
+  if (mapping) {
+    return mapping.m;
+  } else {
+    return null;
+  }
+}
 
 // "https://swapi.dev/api/films/1/" --> Episode_id = 4 (A New Hope)
 // "https://swapi.dev/api/films/2/" --> Episode_id = 5 (The Empire Strikes Back)
